6af2c4767d0345232b0493286b455489360496af
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-10-09 18:19:29 -0400

    Done

--- XamarinIndividualExercise/XamarinIndividualExercise/ViewModels/MainViewModel.cs
+++ XamarinIndividualExercise/XamarinIndividualExercise/ViewModels/MainViewModel.cs
@@ -10,14 +10,14 @@ namespace XamarinIndividualExercise.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        public DeviceOrientation orientation { get; set; }
-        public ICommand GetDeviceOrientation { get; set; }
+        public DeviceOrientation Orientation { get; set; }
+        public ICommand GetOrientationCommand { get; }
         public MainViewModel()
         {
             IDeviceOrientationService service = DependencyService.Get<IDeviceOrientationService>();
-            GetDeviceOrientation = new Command(() =>
+            GetOrientationCommand = new Command(() =>
             {
-                orientation = service.GetOrientation();
+                Orientation = service.GetOrientation();
             });
         }
     }
