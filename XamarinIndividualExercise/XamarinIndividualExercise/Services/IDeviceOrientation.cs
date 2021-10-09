97c88b9c95d104b223cae849bfca0138393d9bb7
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-10-09 15:17:35 -0400

    Add project files.

---
+++ XamarinIndividualExercise/XamarinIndividualExercise/Services/IDeviceOrientation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials.Internals;
+
+namespace XamarinIndividualExercise.Services
+{
+    public interface IDeviceOrientation
+    {
+        IDeviceOrientation GetOrientation();
+    }
+}
---
+++ XamarinIndividualExercise/XamarinIndividualExercise/ViewModels/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace XamarinIndividualExercise.ViewModels
+{
+    public abstract class BaseViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
